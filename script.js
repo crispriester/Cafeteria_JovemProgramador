4b32140d000dd60992b72168add7735a2fa32d29
JoaoMS97 <joaobusht@gmail.com>
2022-02-17 23:12:45 -0300

    Implementções na página 1

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("1").style.opacity = "0.7"
+
+function escondePromocao() {
+    document.getElementById("promocoes").style.display = "none"
+    document.getElementById("melhores-cardapio").style.display = "flex"
+    document.getElementById("1").style.opacity = "0.7"
+    document.getElementById("2").style.backgroundColor = "burlywood"
+    document.getElementById("2").style.opacity = "1"
+}
+document.getElementById("1").addEventListener("click", escondePromocao)
+
+function melhoresCardapio() {
+    document.getElementById("promocoes").style.display = "flex"
+    document.getElementById("melhores-cardapio").style.display = "none"
+    document.getElementById("2").style.opacity = "0.7"
+    document.getElementById("1").style.backgroundColor = "burlywood"
+    document.getElementById("1").style.opacity = "1"
+}
+document.getElementById("2").addEventListener("click", melhoresCardapio)
