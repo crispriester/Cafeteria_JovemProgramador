678fb87fafd12065760e7d7c76f4d4a890f4e08f
JoaoMS97 <joaobusht@gmail.com>
2022-03-01 20:56:10 -0300

    Validação adicional para CEP

--- src/pages/Produtos/script.js
+++ src/pages/Produtos/script.js
@@ -10,8 +10,8 @@ function escondePromocao() {
 document.getElementById("1").addEventListener("click", escondePromocao)
 
 function melhoresCardapio() {
-    document.getElementById("promocoes").style.display = "flex"
     document.getElementById("melhores-cardapio").style.display = "none"
+    document.getElementById("promocoes").style.display = "flex"
     document.getElementById("1").style.opacity = "0.7"
     document.getElementById("2").style.backgroundColor = "burlywood"
     document.getElementById("2").style.opacity = "1"
